149fa83b837138924b12b018b0992cb55ecb3b56
hadibd <jhalhadi@gmail.com>
2021-11-30 17:16:49 +0600

    day-16 opacity

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+class ProfilePage extends StatefulWidget {
+   ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  double opc = 0.1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purpleAccent,
-      body: Center(
-        child: Text('Profile Page', style: TextStyle(fontSize: 22, color: Colors.white),),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: (){
+                setState(() {
+                  if(opc<=1){
+                    opc += 0.1;
+                  }
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(24),
+                child: Opacity(
+                  opacity: opc,
+                  child: Image.network('https://images.pexels.com/photos/8265680/pexels-photo-8265680.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                )
+              ),
+            ),
+          GestureDetector(
+            onTap: (){
+              print('Text Clicked');
+                },
+                child: Text('Profile Page', style: TextStyle(fontSize: 22, color: Colors.white),))
+          ],
+        ),
       ),
     );
   }
